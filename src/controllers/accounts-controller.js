9fd78223c7bb43124dba547116e2f4a9311089e0
RGoedert <renato.goedert@gmail.com>
2023-02-20 16:59:56 +0000

    Lab 0.3.1

--- src/controllers/accounts-controller.js
+++ src/controllers/accounts-controller.js
@@ -1,5 +1,6 @@
 import { db } from "../models/db.js";
 import { UserSpec } from "../models/joi-schemas.js";
+import { UserCredentialsSpec } from "../models/joi-schemas.js";
 
 export const accountsController = {
   index: {
@@ -41,6 +42,13 @@ export const accountsController = {
 
   login: {
     auth: false,
+    validate: {
+      payload: UserCredentialsSpec,
+      options: { abortEarly: false },
+      failAction: function(request, h, error) {
+        return h.view("signup-view", { title: "Sign up error", errors: error.details }).takeover().code(400);
+      },
+    },
     handler: async function (request, h) {
       const { email, password } = request.payload;
       const user = await db.userStore.getUserByEmail(email);
--- src/models/joi-schemas.js
+++ src/models/joi-schemas.js
@@ -6,3 +6,19 @@ export const UserSpec = {
   email: Joi.string().email().required(),
   password: Joi.string().required(),
 };
+
+export const UserCredentialsSpec = {
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+};
+
+export const TrackSpec = {
+  title: Joi.string().required(),
+  artist: Joi.string().required(),
+  duration: Joi.number().required(),
+
+};
+
+export const PlaylistSpec = {
+  title: Joi.string().required(),
+};
