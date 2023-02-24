34511f83275adf5b5a0e65d219afdfbe5cba0aa3
RGoedert <renato.goedert@gmail.com>
2023-02-24 20:04:50 +0000

    playlist api mongo + test

--- test/api/playtime-service.js
+++ test/api/playtime-service.js
@@ -2,12 +2,53 @@ import axios from "axios";
 
 import { serviceUrl } from "../fixtures.js";
 
-
 export const playtimeService = {
-    playtimeUrl: serviceUrl,
+  playtimeUrl: serviceUrl,
 
-    async createUser(user) {
-        const res = await axios.post(`${this.playtimeUrl}/api/users`, user);
-        return res.data;
-      }
+  async createUser(user) {
+    const res = await axios.post(`${this.playtimeUrl}/api/users`, user);
+    return res.data;
+  },
+
+  async getUser(id) {
+    const res = await axios.get(`${this.playtimeUrl}/api/users/${id}`);
+    return res.data;
+  },
+
+  async getAllUsers() {
+    const res = await axios.get(`${this.playtimeUrl}/api/users`);
+    return res.data;
+  },
+
+  async deleteAllUsers() {
+    const res = await axios.delete(`${this.playtimeUrl}/api/users`);
+    return res.data;
+  },
+
+  async createPlaylist(playlist) {
+    const res = await axios.post(`${this.playtimeUrl}/api/playlists`, playlist);
+    return res.data;
+  },
+
+  async deleteAllPlaylists() {
+    const response = await axios.delete(`${this.playtimeUrl}/api/playlists`);
+    return response.data;
+  },
+
+  async deletePlaylist(id) {
+    const response = await axios.delete(`${this.playtimeUrl}/api/playlists/${id}`);
+    return response;
+  },
+
+  async getAllPlaylists() {
+    const res = await axios.get(`${this.playtimeUrl}/api/playlists`);
+    return res.data;
+  },
+
+  async getPlaylist(id) {
+    const res = await axios.get(`${this.playtimeUrl}/api/playlists/${id}`);
+    return res.data;
+  },
 };
+
+
